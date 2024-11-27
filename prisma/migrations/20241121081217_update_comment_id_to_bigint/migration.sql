c460fe25de64b50de93d98ce34f9916faca75326
hyogeun <stonepagoda8@gmail.com>
2024-11-27 22:19:06 +0900

    Add sequence creation for Comment id

--- prisma/migrations/20241121081217_update_comment_id_to_bigint/migration.sql
+++ prisma/migrations/20241121081217_update_comment_id_to_bigint/migration.sql
@@ -4,8 +4,12 @@
   - The primary key for the `Comment` table will be changed. If it partially fails, the table could be left without primary key constraint.
 
 */
+CREATE SEQUENCE IF NOT EXISTS comment_id_seq;
+
 -- AlterTable
 ALTER TABLE "Comment" DROP CONSTRAINT "Comment_pkey",
 ALTER COLUMN "id" SET DATA TYPE BIGINT,
 ALTER COLUMN "id" SET DEFAULT nextval('comment_id_seq'::regclass),
 ADD CONSTRAINT "Comment_pkey" PRIMARY KEY ("id");
+
+SELECT setval('comment_id_seq', (SELECT MAX(id) FROM "Comment"));
