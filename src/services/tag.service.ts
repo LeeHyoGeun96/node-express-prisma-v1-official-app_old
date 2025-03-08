f1a2457c231fe6e681d6d8260af7118037cc2023
hyogeun <stonepagoda8@gmail.com>
2025-03-08 11:05:32 +0900

    feat: 태그 로직 변경
    - 로그인 한 사용자의 태그 앞에 3개 나머지는 기존 로직

--- src/services/tag.service.ts
+++ src/services/tag.service.ts
@@ -1,28 +1,60 @@
 import prisma from '../../prisma/prisma-client';
 
-const getTags = async (username?: string): Promise<string[]> => {
-  const whereCondition = username
-    ? {
+const getTags = async (userId?: number): Promise<string[]> => {
+  if (userId) {
+    // 1. 로그인한 사용자의 태그 (최대 3개)
+    const userTags = await prisma.tag.findMany({
+      where: {
         articles: {
           some: {
             author: {
-              username,
+              id: userId,
             },
           },
         },
-      }
-    : {
+      },
+      select: { name: true },
+      orderBy: {
+        articles: {
+          _count: 'desc',
+        },
+      },
+      take: 3,
+    });
+
+    // 2. 나머지 태그들 (이미 가져온 태그 제외)
+    const otherTags = await prisma.tag.findMany({
+      where: {
+        name: {
+          notIn: userTags.map(tag => tag.name),
+        },
         articles: {
           some: {},
         },
-      };
+      },
+      select: { name: true },
+      orderBy: {
+        articles: {
+          _count: 'desc',
+        },
+      },
+      take: 10 - userTags.length,
+    });
 
-  const tags = await prisma.tag.groupBy({
-    where: whereCondition,
-    by: ['name'],
+    return [...userTags, ...otherTags].map(tag => tag.name);
+  }
+
+  // 비로그인 시 기존 로직
+  const tags = await prisma.tag.findMany({
+    where: {
+      articles: {
+        some: {},
+      },
+    },
+    select: { name: true },
     orderBy: {
-      _count: {
-        name: 'desc',
+      articles: {
+        _count: 'desc',
       },
     },
     take: 10,
